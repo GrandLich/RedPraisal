e51c0d77cfeb4d0bdffd569d876d2a35c8a14783
GrandLich <seregastepanishev@yandex.ru>
2022-02-13 02:33:41 +0300

    Initial commit

---
+++ src/main/java/io/github/grandlich/redpraisal/RedPraisalApplication.java
@@ -0,0 +1,13 @@
+package io.github.grandlich.redpraisal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedPraisalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedPraisalApplication.class, args);
+	}
+
+}
